f2104cff91a38e906d9cf87395dda5c943c7d710
nataliia <102330868+natalikulybina@users.noreply.github.com>
2022-11-14 19:32:24 +0200

    hm 16

--- src/index.js
+++ src/index.js
@@ -11,4 +11,40 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+fastify.post('/uppercase', (request, reply) => {
+  if (request.body.toLowerCase().includes('fuck')) {
+    return reply.status(403).send('unresolved');
+  } else {
+    return reply.send(request.body.toUpperCase());
+  }
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  if (request.body.toLowerCase().includes('fuck')) {
+    return reply.status(403).send('unresolved');
+  } else {
+    return reply.send(request.body.toLowerCase());
+  }
+});
+
+fastify.get('/user/:id', (request, reply) => {
+  const {id } = request.params;
+
+  if (id in users) {
+    return reply.send(users[id]);
+  } else {
+    return reply.status(400).send('User not exist');
+  }
+});
+
+fastify.get('/users', (request, reply) => {
+  const { filter, value } = request.query;
+  const result = Object.values(users).filter((users) => { users[filter] === filter || users[filter] === value;
+  });
+  if (!filter || !value) {
+    return reply.send(Object.values(users));
+  }
+  reply.send(result);
+});
+
 export default fastify;
